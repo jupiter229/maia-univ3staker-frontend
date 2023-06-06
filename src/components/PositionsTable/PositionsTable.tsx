ec8b47de9f45d02e9aa6fc1feaa7f64bacb3f69a
0xLightt <0xlightt@protonmail.com>
2023-06-06 18:47:07 +0100

    Add liquidity button

--- src/components/PositionsTable/PositionsTable.tsx
+++ src/components/PositionsTable/PositionsTable.tsx
@@ -2,6 +2,7 @@
 import { useWeb3 } from "@/hooks";
 import { IPosition } from "@/types";
 import { formatBigInt, formatDateDiff } from "@/utils";
+import Link from "next/link";
 import { useMemo } from "react";
 import { ConnectWallet } from "../ConnectWallet";
 import { Table } from "../Table";
@@ -50,9 +51,31 @@ export const PositionsTable: React.FC<IProps> = ({
     ],
     [incentiveId]
   );
+  console.log(data);
   return (
     <div className="flex flex-col gap-4 justify-center items-center text-white w-full">
-      {title && <h5 className="text-lg font-semibold px-6 w-full">{title}</h5>}
+      {title && (
+        <h5 className="text-lg font-semibold px-6 w-full grid grid-cols-5 gap-4">
+          {title}{" "}
+          <Link
+            type="button"
+            target="_blank"
+            href={
+              data !== undefined
+                ? "https://uni.maiadao.io/#/add/" +
+                  data[0].pool.token0.id +
+                  "/" +
+                  data[0].pool.token1.id +
+                  "/" +
+                  data[0].pool.feeTier
+                : "."
+            }
+            class="text-white col-start-5 bg-gradient-to-br from-purple-800 to-blue-800 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg py-2.5 text-center"
+          >
+            Add Liquidity
+          </Link>
+        </h5>
+      )}
       {account ? (
         <Table columns={columns} data={data || []} />
       ) : (
